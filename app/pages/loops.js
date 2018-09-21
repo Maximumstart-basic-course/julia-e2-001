9f3feddc98c3032c9d52b24cc68a66ba95e55472
Юлия Подолян <julia28podolyan@gmail.com>
2018-09-21 11:09:00 +0300

    Исправила ошибки во втором домашнем задании

--- app/pages/loops.js
+++ app/pages/loops.js
@@ -13,7 +13,7 @@ while (k >= 0) {
 
 const isAdmin = true;
 
-isAdmin == true ? alert('Привет, Admin!') : alert('Ты точно не Admin');
+isAdmin === true ? alert('Привет, Admin!') : alert('Ты точно не Admin');
 
 // Перепиши if...else на switch:
 
@@ -21,41 +21,44 @@ const age = +prompt('Сколько тебе лет?', '12');
 
 switch (age) {
 	case 12 :
-		alert('Слишком мало :(');
-		break;
+	alert('Слишком мало :(');
+	break;
+
 	case 16 :
-		alert('Почти достаточно :[');
-		break;
+	alert('Почти достаточно :[');
+	break;
+
 	case 18 :
-		alert('Вот сейчас в самый раз! :)');
-		break;
+	alert('Вот сейчас в самый раз! :)');
+	break;
+	
 	default :
-		alert('Возраст не определен %)');
+	alert('Возраст не определен %)');
 }
 
 // Напиши примитивную проверку логина и пароля, которая 
 // используется в роутерах, опираясь на схему:
 
-const log = prompt ('Введите логин' , '');
+const log = prompt ('Введите логин', '');
 
 if (log === 'Admin') {
-	const pas = prompt ('Введите пароль' , '');
-		if (pas === 'qwerty') {
-			alert ('Добро пожаловать, Admin');
-		} else if (pas === null || pas === '') {
-			alert ('Отказано в доступе');
-		} else {
-			alert ('Введенный пароль неправильный');
-		}
+	const pas = prompt ('Введите пароль', '');
+	if (pas === 'qwerty') {
+		alert ('Добро пожаловать, Admin');
+	} else if (pas === null || pas === '') {
+		alert ('Отказано в доступе');
+	} else {
+		alert ('Введенный пароль неправильный');
+	}
 } else if (log === 'User') {
-	const pas = +prompt ('Введите пароль' , '');
-		if (pas === 12345) {
-			alert ('Добро пожаловать, User');
-		} else if (pas === 0) {
-			alert ('Отказано в доступе');
-		} else {
-			alert ('Введенный пароль неправильный');
-		}
+	const pas = +prompt ('Введите пароль', '');
+	if (pas === 12345) {
+		alert ('Добро пожаловать, User');
+	} else if (pas === 0) {
+		alert ('Отказано в доступе');
+	} else {
+		alert ('Введенный пароль неправильный');
+	}
 } else {
 	alert ('Такого логина нет в системе');
 }
